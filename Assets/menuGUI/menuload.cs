e6fc103f4ea9471225a79c5b2ea0236ac77d6bc4
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-02-10 23:26:59 -0500

    added main menu
    textmeshpro installed
    semi functunal main menu
    setting scene prototyped

---
+++ Assets/menuGUI/menuload.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class loadscene : MonoBehaviour
+{
+
+
+    public void LoadByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+        //Application.LoadLevelAdditive(sceneIndex);
+    }
+}
